3025851e2a7d10b49b75005a365f118df9862b89
FernandesLr <leo1ramosf@gmail.com>
2025-06-14 17:43:16 -0300

    Feat: integra API de produtos e renderiza lista com componente Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
-import './App.css'
-import Header from './components/Header'
+import './App.css';
+import Header from './components/Header';
+import Card from './components/Card';
+import { api } from './services/api.js';
+import { useEffect, useState } from 'react';
 
 function App() {
-  
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    api.get('/produtos')
+      .then(response => {
+        setProducts(response.data);
+      })
+      .catch(error => {
+        console.error('Erro ao buscar produtos:', error);
+      });
+  }, []);
 
   return (
     <>
-      
+      <Header />
+      <div className='product-wrapper'>
+        {products.map((item) => (
+          <Card key={item.id} product={item} />
+        ))}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
